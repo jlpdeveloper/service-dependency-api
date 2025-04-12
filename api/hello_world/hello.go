67e21f05416a424cd5b6f2a19ba0323ac75f3505
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-11 21:23:37 -0500

    fix: remove unused function

--- api/hello_world/hello.go
+++ api/hello_world/hello.go
@@ -4,10 +4,6 @@ import (
 	"net/http"
 )
 
-func SayHelloWorld() {
-	println("hello worlds")
-}
-
 func HelloWorld(rw http.ResponseWriter, req *http.Request) {
 	name := req.URL.Query().Get("name")
 	response := "hello world"
