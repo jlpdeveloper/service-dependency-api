5837605b0e8679c18adaf666626d3b369a8619ec
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-12 19:40:22 -0500

    feat: add new routes to mux

--- api/routes/router.go
+++ api/routes/router.go
@@ -4,9 +4,12 @@ import (
 	"log"
 	"net/http"
 	"service-dependency-api/api/hello_world"
+	"service-dependency-api/api/system"
 )
 
 func SetupRoutes(mux *http.ServeMux) {
 	log.Println("Setting up Routes")
 	mux.HandleFunc("GET /helloworld", hello_world.HelloWorld)
+	mux.HandleFunc("GET /time", system.GetTime)
+	mux.HandleFunc("GET /database", system.GetDbAddress)
 }
