f766ea64c2c3e80da89779365921373c8925fd3d
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-12 19:39:43 -0500

    feat: adds get time and get database call handlers

---
+++ api/system/system.go
@@ -0,0 +1,33 @@
+package system
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"service-dependency-api/internal/config"
+	"time"
+)
+
+func GetTime(rw http.ResponseWriter, req *http.Request) {
+	now := time.Now().Format("2006-01-02 15:04:05")
+	_, err := io.WriteString(rw, now)
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "text/plain")
+}
+
+func GetDbAddress(rw http.ResponseWriter, req *http.Request) {
+	url := config.GetConfigValue("neo4j_url")
+
+	if url == "" {
+		http.Error(rw, "Could not find environment variable", http.StatusInternalServerError)
+		return
+	}
+	_, err := io.WriteString(rw, url)
+	if err != nil {
+		log.Println(err)
+	}
+}
