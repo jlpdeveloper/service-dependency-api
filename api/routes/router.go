7abdd936b9dcf001cc09e792cd39099f0c85e292
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-13 20:48:31 -0500

    feat adds routing for post

--- api/routes/router.go
+++ api/routes/router.go
@@ -1,15 +1,20 @@
 package routes
 
 import (
+	"context"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"log"
 	"net/http"
 	"service-dependency-api/api/hello_world"
+	"service-dependency-api/api/services"
 	"service-dependency-api/api/system"
 )
 
-func SetupRoutes(mux *http.ServeMux) {
+func SetupRoutes(mux *http.ServeMux, ctx *context.Context, driver *neo4j.DriverWithContext) {
 	log.Println("Setting up Routes")
 	mux.HandleFunc("GET /helloworld", hello_world.HelloWorld)
 	mux.HandleFunc("GET /time", system.GetTime)
 	mux.HandleFunc("GET /database", system.GetDbAddress)
+
+	services.Register(mux, driver, ctx)
 }
--- api/services/registration.go
+++ api/services/registration.go
@@ -9,13 +9,19 @@ import (
 func Register(mux *http.ServeMux, driver *neo4j.DriverWithContext, ctx *context.Context) {
 
 	serviceRepo := ServiceNeo4jService{
-		ctx:    ctx,
-		Driver: driver,
+		ctx:    *ctx,
+		Driver: *driver,
 	}
-	getAllHandler := GetAllServicesHandler{
-		Path:       "GET /services",
+	//getAllHandler := GetAllServicesHandler{
+	//	Path:       "GET /services",
+	//	Repository: &serviceRepo,
+	//}
+	//
+	//getAllHandler.Register(mux)
+
+	postHandler := POSTServicesHandler{
+		Path:       "POST /services",
 		Repository: &serviceRepo,
 	}
-
-	getAllHandler.Register(mux)
+	postHandler.Register(mux)
 }
