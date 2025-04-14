221a3cae31516a30d54464aa09c15476d10d892b
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-13 20:47:22 -0500

    feat: add CreateService implementation

--- api/services/repository.go
+++ api/services/repository.go
@@ -6,15 +6,15 @@ import (
 )
 
 type Service struct {
-	id          int    `json:"id"`
-	name        string `json:"name"`
-	serviceType string `json:"type"`
-	description string `json:"description"`
+	Id          string `json:"id,omitempty"`
+	Name        string `json:"name"`
+	ServiceType string `json:"type"`
+	Description string `json:"description"`
 }
 
 type ServiceRepository interface {
 	//GetAllServices(page int) ([]Service, error)
-	//CreateService(service Service) (int, error)
+	CreateService(service Service) (string, error)
 	//UpdateService(service Service) error
 	//DeleteService(service Service) error
 	//GetServiceById(id int) (Service, error)
@@ -22,8 +22,8 @@ type ServiceRepository interface {
 
 // the type implements the interface
 type ServiceNeo4jService struct {
-	Driver *neo4j.DriverWithContext
-	ctx    *context.Context
+	Driver neo4j.DriverWithContext
+	ctx    context.Context
 }
 
 //https://github.com/neo4j-examples/golang-neo4j-realworld-example/blob/main/pkg/users/login.go
@@ -34,3 +34,35 @@ type ServiceNeo4jService struct {
 //		err = session.Close(d.ctx)
 //	}()
 //}
+
+func (d *ServiceNeo4jService) CreateService(service Service) (id string, err error) {
+	session := d.Driver.NewSession(d.ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer func() {
+		err = session.Close(d.ctx)
+	}()
+	createServiceTransaction := func(tx neo4j.ManagedTransaction) (any, error) {
+		result, err := tx.Run(
+			d.ctx, `
+        CREATE (n: Service {id: randomuuid(), createdDate: datetime(), name: $name, type: $type, description: $description})
+        RETURN n.id AS id
+        `, map[string]any{
+				"name":        service.Name,
+				"type":        service.ServiceType,
+				"description": service.Description,
+			})
+		if err != nil {
+			return "", err
+		}
+		svc, err := result.Single(d.ctx)
+		if err != nil {
+			return "", err
+		}
+		svcId, _ := svc.AsMap()["id"]
+		return svcId.(string), err
+	}
+	newId, insertErr := session.ExecuteWrite(d.ctx, createServiceTransaction)
+	if insertErr != nil {
+		return "", insertErr
+	}
+	return newId.(string), nil
+}
