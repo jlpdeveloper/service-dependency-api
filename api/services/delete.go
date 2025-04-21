fcc542bb3c56e03e6302bf2a9a914de588b5f88d
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-04-20 20:08:40 -0500

    use new custom http error appropriately

--- api/services/delete.go
+++ api/services/delete.go
@@ -1,18 +1,30 @@
 package services
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"service-dependency-api/internal"
+)
 
 func (u *ServiceCallsHandler) DeleteServiceById(rw http.ResponseWriter, req *http.Request) {
 	id, ok := u.IdValidator("id", req)
+	log.Println("Request received - DeleteServiceById - " + id)
 	if !ok {
 		http.Error(rw, "Invalid Request", http.StatusBadRequest)
+		log.Println("Invalid Request - " + id)
 		return
 	}
 
 	err := u.Repository.DeleteService(id)
-
+	var httpErr *internal.HTTPError
 	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		if errors.As(err, &httpErr) {
+			http.Error(rw, httpErr.Error(), httpErr.Status)
+		} else {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			log.Println("Error deleting service: " + err.Error())
+		}
 		return
 	}
 	rw.WriteHeader(http.StatusNoContent)
