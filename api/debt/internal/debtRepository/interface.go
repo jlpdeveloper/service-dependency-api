dc22a8991ee05d5d7b57c169729af87e085abbb5
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-05-17 20:49:48 -0500

    feat: add interface and neo4j implementation for debt

---
+++ api/debt/internal/debtRepository/interface.go
@@ -0,0 +1,30 @@
+package debtRepository
+
+import (
+	"context"
+	"errors"
+	"service-dependency-api/internal"
+)
+
+type Repository interface {
+	CreateDebtItem(ctx context.Context, debt Debt) error
+	UpdateStatus(ctx context.Context, id, status string) error
+}
+
+type Debt struct {
+	ServiceId   string `json:"serviceId"`
+	Type        string `json:"type"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
+func (d Debt) Validate() error {
+	if d.Status == "" {
+		d.Status = "pending"
+	}
+	if !internal.DebtTypes.IsMember(d.Type) || !internal.DebtStatus.IsMember(d.Status) {
+		return errors.New("invalid debt")
+	}
+	return nil
+}
---
+++ api/debt/internal/debtRepository/update.go
@@ -0,0 +1,8 @@
+package debtRepository
+
+import "context"
+
+func (n Neo4jDebtRepository) UpdateStatus(ctx context.Context, id, status string) error {
+	//TODO implement me
+	panic("implement me")
+}
