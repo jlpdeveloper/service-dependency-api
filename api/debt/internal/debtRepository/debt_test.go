78dbe17f306a7242901891f9da58adf890fa9670
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-05-18 19:29:38 -0500

    tests: add tests for debt validation

---
+++ api/debt/internal/debtRepository/debt_test.go
@@ -0,0 +1,49 @@
+package debtRepository
+
+import "testing"
+
+func TestDebt_ValidateSuccess(t *testing.T) {
+	debt := Debt{
+		Title:     "test",
+		Type:      "code",
+		ServiceId: "test",
+	}
+	err := debt.Validate()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDebt_ValidateFailNoTitle(t *testing.T) {
+	debt := Debt{
+		Type:      "code",
+		ServiceId: "test",
+	}
+	err := debt.Validate()
+	if err == nil {
+		t.Error("Expected error")
+	}
+}
+
+func TestDebt_ValidateFailNoType(t *testing.T) {
+	debt := Debt{
+		Title:     "test",
+		ServiceId: "test",
+	}
+	err := debt.Validate()
+	if err == nil {
+		t.Error("Expected error")
+	}
+}
+
+func TestDebt_ValidateFailIncorrectType(t *testing.T) {
+	debt := Debt{
+		Title:     "test",
+		Type:      "duck",
+		ServiceId: "test",
+	}
+	err := debt.Validate()
+	if err == nil {
+		t.Error("Expected error")
+	}
+}
