94280701e58a4d11a8667f9f3f2d4d621f9d1b3a
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-11-01 17:43:52 -0500

    feat: implement UpdateTeam repository method with tests
    - Add Neo4j transaction to update team details and updated timestamp.
    - Add unit test to validate UpdateTeam functionality with Neo4j test container.

--- neo4jRepositories/teamRepository/update.go
+++ neo4jRepositories/teamRepository/update.go
@@ -2,11 +2,53 @@ package teamRepository
 
 import (
 	"context"
-	"errors"
+	"net/http"
+	"service-dependency-api/internal/customErrors"
 
 	"service-dependency-api/repositories"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func (r Neo4jTeamRepository) UpdateTeam(ctx context.Context, team repositories.Team) error {
-	return errors.New("Not implemented")
+	_, err := r.GetTeam(ctx, team.Id)
+	// Error should be a custom http error already
+	if err != nil {
+		return err
+	}
+
+	updateTeamTransaction := func(tx neo4j.ManagedTransaction) (any, error) {
+		updateResult, updateErr := tx.Run(ctx, `
+			MATCH (s:Team)
+			WHERE s.id = $id
+			SET s.name = $name,
+				s.updated = datetime()
+			RETURN s
+		`, map[string]any{
+			"id":   team.Id,
+			"name": team.Name,
+		})
+
+		if updateErr != nil {
+			return nil, customErrors.HTTPError{
+				Status: http.StatusInternalServerError,
+				Msg:    err.Error(),
+			}
+		}
+
+		// Confirm update was successful
+		if !updateResult.Next(ctx) {
+			err = customErrors.HTTPError{
+				Status: http.StatusInternalServerError,
+				Msg:    "update Team failed",
+			}
+		}
+		return nil, nil
+	}
+
+	_, err = r.manager.ExecuteWrite(ctx, updateTeamTransaction)
+	if err != nil {
+		return err
+	}
+	return nil
 }
