7c41be92db47bb4d27c8fec6e85904530486dca2
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2025-11-15 19:29:18 -0600

    feat: update MCP server setup with prompts initialization
    - Rename server implementation and tool name for clarity.
    - Add `SetupPrompts` to integrate prompts functionality into the server.

--- mcp/cmd/service-dependency-mcp/main.go
+++ mcp/cmd/service-dependency-mcp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"service-dependency-api/mcp/prompts"
 	"service-dependency-api/mcp/tools"
 
 	"github.com/modelcontextprotocol/go-sdk/mcp"
@@ -10,9 +11,9 @@ import (
 
 func main() {
 	// Create a server with a single tool.
-	server := mcp.NewServer(&mcp.Implementation{Name: "greeter", Version: "v1.0.0"}, nil)
-	mcp.AddTool(server, &mcp.Tool{Name: "greet", Description: "say hi"}, tools.HelloWorld)
-
+	server := mcp.NewServer(&mcp.Implementation{Name: "Service Dependency API", Version: "v1.0.0"}, nil)
+	mcp.AddTool(server, &mcp.Tool{Name: "hello_world", Description: "say hi"}, tools.HelloWorld)
+	prompts.SetupPrompts(server)
 	// Run the server over stdin/stdout, until the client disconnects.
 	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
 		log.Fatal(err)
